467c68c3e5ff60a8bcee43334209128c3e45b03d
mizaellage <mizaeljs830@gmail.com>
2025-08-09 16:36:04 +0000

    SuperTrunfo

--- xadrez.c
+++ xadrez.c
@@ -3,8 +3,52 @@
 int main () {
 
 
-    printf ("Desafio Xadrez!\n");
-    printf ("Novo commit\n");
+    printf ("Desafio do Super Trunfo-países");
+
+    char carta1;
+    char s;
+    int s01;
+   
+
+
+    char carta2;
+    char r;
+    int r02;
+    
+
+
+    printf ("Cadastro da primeira carta");
+    scanf ("%s",&carta1);
+    
+
+    printf ("Cadastro do primeiro Estado\n");
+    scanf ("%s",&s);
+    
+
+    printf ("Cadastro do primeiro código");
+    scanf ("%d", s01);
+
+
+    printf ("Cadastro da segunda carta");
+    scanf ("Cadastro do segundo Estado");
+
+
+    printf ("Cadastro do segundo código");
+    scanf ("%d", r02);
+
+  
+
+
+   
+
+
+
+
+
+
+
+
+
 
 
 
